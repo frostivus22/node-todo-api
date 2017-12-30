b69c61a9c54af1025d96f2f0c7a27222695969ec
unknown <nicksolante18@gmail.com>
2017-12-30 23:07:35 +1100

    new get commands

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+var {ObjectID} = require('mongodb');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -29,7 +30,35 @@ app.get('/todos', (req, res) => {
   });
 });
 
-app.listen(4000,() => {
+// GET /todos/12341234 --> how to let the 1234 part be dynamically chosen by the Users
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+
+  //validate id using isValid
+   // if error insues return error 404 - send back empty bodyParser
+   if(!ObjectID.isValid(id)){
+     return res.status(404).send();
+   }
+
+   //findById
+    //success case
+      // if todo - send it back
+      //if no todo - send back 404 with empty body
+    //error case
+      //send back 400 and send empty body back
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+      res.status(400).send();
+    })
+});
+
+app.listen(3000,() => {
   console.log('starting on port 3000');
 });
 
