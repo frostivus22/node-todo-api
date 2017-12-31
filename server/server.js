58fecddc9aa607b31b7bd641f5559f3bb7b03da8
unknown <nicksolante18@gmail.com>
2017-12-31 15:26:50 +1100

    heroku ready app

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 var {ObjectID} = require('mongodb');
 var express = require('express');
 var bodyParser = require('body-parser');
-
 var {mongoose} =  require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 
 var app = express();
+
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json());
 
@@ -58,8 +59,8 @@ app.get('/todos/:id', (req, res) => {
     })
 });
 
-app.listen(3000,() => {
-  console.log('starting on port 3000');
+app.listen(port,() => {
+  console.log(`started up at port ${port}`);
 });
 
 
